d3617b79f8fd77d9ba3be9436a76274efcae6f90
Ross Clare <rossclare11589@gmail.com>
2019-10-23 10:20:40 -0400

    changed the login html so that it has a button that pulls from database, not permanent but should make implementing pulling easier

--- src/main/java/com/revature/servlets/LoginServlet.java
+++ src/main/java/com/revature/servlets/LoginServlet.java
@@ -1,14 +1,21 @@
 package com.revature.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.dao.UserDaoImpl;
 import com.revature.pojo.User;
 import com.revature.service.UserService;
 import com.revature.service.UserServiceImpl;
+import com.revature.util.ConnectionFactory;
 
 import static com.revature.util.LoggerUtil.*;
 
@@ -17,7 +24,7 @@ import static com.revature.util.LoggerUtil.*;
  */
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+	private static UserDaoImpl userDao = new UserDaoImpl();
 	private static UserServiceImpl userService = new UserServiceImpl();
 	
 	
@@ -34,24 +41,45 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("newvalue");
 		
+		ObjectMapper om = new ObjectMapper();
+
+		String name = request.getPathInfo();
+
+		if (name != null && !"".equals(name.substring(1))) {
+			response.getWriter().write(om.writeValueAsString(userService.getUserByUsername(name.substring(1))));
+		} else {
+
+		List<User> userList = userService.getAllUsers();
+
+			response.getWriter().write(om.writeValueAsString(userList));
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("test");
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		debug(username + ".");
-		debug(password + ".");
+		info(username + ".");
+		info(password + ".");
 		User user = userService.loginUser(username, password);
+		User user2 = userDao.getUser(username);
+		String dbUsername = user2.getUsername();
+			
+		System.out.println("username: " + dbUsername);
 		
+		info(user.getFirstName() + ".");
 		if (user != null) {
-			 debug("Successfully got: " + user.getFirstName());
+			 
 			 request.getSession().setAttribute("user", user);
-			 if (user.getUsername().equals("testUser")) {
-				 debug("verifying correct user");
+			 if (user.getUsername().equals(dbUsername)) {
+				 
 				 response.sendRedirect("tuitionform.html");
 			 } else {
 				 //response.sendRedirect("emp"); 
