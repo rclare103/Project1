23a6383fa8f3160a4e5b0d088399dd0f70519f15
Robert Li <bobli2011@gmail.com>
2019-10-25 05:16:36 -0400

    changing loginservice

--- src/main/java/com/revature/service/UserServiceImpl.java
+++ src/main/java/com/revature/service/UserServiceImpl.java
@@ -8,26 +8,31 @@ import com.revature.dao.UserDaoImpl;
 import com.revature.dao.reDaoImpl;
 import com.revature.pojo.Reimbursement;
 import com.revature.pojo.User;
+import static com.revature.util.LoggerUtil.*;
 
 public class UserServiceImpl implements UserService {
 	private static UserDaoImpl userDao = new UserDaoImpl();
 	private static reDaoImpl reDao = new reDaoImpl();
-	
-	
+
 	@Override
 	public User loginUser(String username, String password) {
-		System.out.println("Attempted login with credentials: Username - " + username + " Password - " + password);
-		
+		debug("Attempted login with creds: Username: " + username + " Password: " + password);
+
 		User user = userDao.getUser(username);
-		
-		if ((user != null) && (user.getPassword().equals(password))) {
+
+		System.out.println("User: " + user);
+
+		if (((user.getUsername() != null) && (user.getPassword() != null)) && (user.getPassword().equals(password))) {
+			debug("Checking if Username: " + user.getUsername() + " Password: " + user.getPassword());
 			return user;
+
 		}
-		
+		debug("Checking if Username: " + user.getUsername() + " Password: " + user.getPassword());
 		return null;
 	}
-	
-	
+
+
+
 	@Override
 	public User getUserByUsername(String username) {
 		return userDao.getUser(username);
